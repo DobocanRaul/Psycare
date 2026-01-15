21174aabb519b1177150d6ada2d9b9be2c9e626f
Dobocan-Raul <dobocan83@gmail.com>
2026-01-15 22:21:33 +0200

    Added Admin Page with requests and posts

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,6 +5,7 @@ import LoginPage from './loginPage/LoginPage.tsx'
 import RegisterPage from './registerPage/registerPage.tsx'
 import NotFoundPage from './notFoundPage/notFoundPage.tsx'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import AdminPage from './adminPage/adminPage.tsx'
 
 document.title="Psycare"
 
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Navigate to="/login" replace />}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/admin" element={<AdminPage/>}/>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
